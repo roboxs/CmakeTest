b4420d96e07145c37f28b209edaa0e0c3e8259ac
roboxs <dux1angyu@163.com>
2025-06-29 13:03:34 +0800

    优化build目录创建

--- script/build.sh
+++ script/build.sh
@@ -8,6 +8,13 @@ cd ${outdir}/../install
 rm -rf *
 mkdir bin lib inc src
 
+#build
+cd ${outdir}/..
+if [ -d build ]; then
+    echo "build already existed"
+else
+    mkdir build
+fi
 cd ${outdir}/../build
 pwd
 rm -rf *
